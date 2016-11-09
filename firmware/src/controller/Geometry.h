57ae895deb1a2fdddd25a5b09f166ba9c96140d0
idt12312 <hd64_3048f@yahoo.co.jp>
2016-11-10 00:25:50 +0900

    Velocityの足し算引き算を実装

--- firmware/src/controller/Geometry.h
+++ firmware/src/controller/Geometry.h
@@ -18,6 +18,16 @@ struct Velocity {
 
 	Velocity(float _v=0.0f, float _omega=0.0f)
 		:v(_v), omega(_omega){}
+
+	Velocity operator+(const Velocity lhs) const
+	{
+		return Velocity(this->v + lhs.v, this->omega + lhs.omega);
+	}
+
+	Velocity operator-(const Velocity lhs) const
+	{
+		return Velocity(this->v - lhs.v, this->omega - lhs.omega);
+	}
 };
 
 
@@ -28,23 +38,6 @@ struct Position {
 
 	Position(float _x=0.0f, float _y=0.0f, float _theta=PI/2)
 		:x(_x), y(_y), theta(_theta) {}
-
-	static float distance(const Position &pos1, const Position &pos2)
-	{
-		float dist;
-		const float dx = pos2.x - pos1.x;
-		const float dy = pos2.y - pos1.y;
-		arm_sqrt_f32(dx*dx + dy*dy, &dist);
-
-		return dist;
-	}
-
-	static float look_at(const Position &from, const Position &at)
-	{
-		const float dx = at.x - from.x;
-		const float dy = at.y - from.y;
-		return std::atan2(dy, dx);
-	}
 };
 
 
