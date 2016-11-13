92ddfde341e71a5af50753b5b0f16e3ee01ad084
idt12312 <hd64_3048f@yahoo.co.jp>
2016-11-13 17:45:45 +0900

    前壁センサの判定に使う閾値を左右に壁があるときとないときで変えられるように修正

--- firmware/src/WallDetect.h
+++ firmware/src/WallDetect.h
@@ -30,17 +30,25 @@ public:
 
 	WallInfo update(const QuadratureDemodulator::Result &sensor_value)
 	{
-		// 前壁の判定は左右の前壁センサの値の平均値を用いる
 		// センサが閾値以下 -> カウンタをクリア
 		// センサが閾値以上 -> カウンタをWALL_DETECT_CNTで飽和させながらカウントアップ
-		if ((sensor_value.sensor1 + sensor_value.sensor4)/2 < WALL_DETECT_FRONT_THRETHOLD) front_cnt = 0;
-		else front_cnt = std::min(front_cnt+1, WALL_DETECT_CNT);
 
 		if (sensor_value.sensor2 < WALL_DETECT_RL_THREHOLD) left_cnt = 0;
 		else left_cnt = std::min(left_cnt+1, WALL_DETECT_CNT);
 
 		if (sensor_value.sensor3 < WALL_DETECT_RL_THREHOLD) right_cnt = 0;
 		else right_cnt = std::min(right_cnt+1, WALL_DETECT_CNT);
+
+		// 前壁の判定は左右の前壁センサの値の平均値を用いる
+		// 前壁は横に壁がある時とない時で閾値を変える
+		if (sensor_value.sensor2 > WALL_DETECT_RL_THREHOLD && sensor_value.sensor3 > WALL_DETECT_RL_THREHOLD) {
+			if ((sensor_value.sensor1 + sensor_value.sensor4)/2 < WALL_DETECT_FRONT_THRETHOLD_WITH_SIDE) front_cnt = 0;
+			else front_cnt = std::min(front_cnt+1, WALL_DETECT_CNT);
+		}
+		else {
+			if ((sensor_value.sensor1 + sensor_value.sensor4)/2 < WALL_DETECT_FRONT_THRETHOLD_NOSIDE) front_cnt = 0;
+			else front_cnt = std::min(front_cnt+1, WALL_DETECT_CNT);
+		}
 
 		WallInfo wall_info;
 		wall_info.front = front_cnt == WALL_DETECT_CNT;
